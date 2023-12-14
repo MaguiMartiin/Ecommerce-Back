8c05318f84adbbf3512aac39f8cb8f79cf12fc56
Magui Martin <magalimartin02@gmail.com>
2023-12-14 19:25:53 -0300

    cambiando estructura

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
-const PORT = 3001; // Puedes cambiar el número de puerto si lo deseas
+const app = require('./src/app')
+const PORT = 3001; 
 
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo desde Express!');
 });
 
-// Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
