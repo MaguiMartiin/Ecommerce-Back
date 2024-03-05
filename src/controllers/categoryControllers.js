3e7812a21f19417ced84f22aa6723a21ce153e3a
Magui Martin <magalimartin02@gmail.com>
2024-03-05 20:18:54 -0300

    agregando los productos en categoria

--- src/controllers/categoryControllers.js
+++ src/controllers/categoryControllers.js
@@ -1,6 +1,11 @@
 const {Category, Products} = require('../db')
 
-const getAllCategories = async  () => {return await Category.findAll({order: [['name', 'ASC']] })}
+const getAllCategories = async  () => {return await Category.findAll({order: [['name', 'ASC']], include: [
+    {
+      model: Products,
+      through: { attributes: [] },
+    },
+  ], })}
 
 const createCategory = async (name) => {return await Category.create({ name })}
 
