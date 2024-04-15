592fc7b03e2b8f0304c5bdc83d48c379f95ea3ee
Magui Martin <magalimartin02@gmail.com>
2024-04-15 15:39:08 -0300

    Hashing password

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,9 @@
 const { Users } = require('../db')
+const bcrypt = require('bcrypt')
 
 const newUser = async (name, email, password) =>  {
-    const user = await Users.create({name, email, password})
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await Users.create({name, email, password: hashedPassword})
     return user
 }
 
